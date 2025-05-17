56197b4c2b8c89c502da7bf5f20f8766acd7f7b1
janav-cloud <janavcloud@gmail.com>
2025-05-17 18:08:17 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.36%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,18 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+        table = []
+        for num in nums:
+            if num == val:
+                continue
+            else:
+                table.append(num)
+
+        size = len(table)
+        for i in range(0, n):
+            if i < size:
+                nums[i] = table[i]
+            else:
+                nums[i] = '_'
+
+        return size
